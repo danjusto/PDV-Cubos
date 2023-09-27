1de66cac167738f7efd5f89432aff2e9a303f9cc
Daniel Justo <justodaniel29@gmail.com>
2023-09-27 16:09:33 -0300

    feat: create connection with postgres using knex and create migrations and seed

---
+++ src/database/migrations/20230927184200_create_table_categories.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('categories', (table) => {
+        table.increments('id').primary();
+        table.string('descricao', 255).notNullable();
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('categories')
+};
