8c0f60b33e3034e2908530d42fb1936840475c8a
Daniel Justo <justodaniel29@gmail.com>
2023-09-28 16:54:24 -0300

    feat: add middleware to create user and encrypt password

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { findByEmail, insertUser } = require('../repositories/userRepository');
 const AppError = require('../errors/AppError');
 
@@ -6,8 +7,8 @@ const executeCreate = async (nome, email, senha) => {
     if(checkEmailExists) {
         throw new AppError("Email already exists.", 400);
     }
-
-    const userCreated = await insertUser(nome, email, senha);
+    const encryptPassword = await bcrypt.hash(senha, 10);
+    const userCreated = await insertUser(nome, email, encryptPassword);
     return userCreated[0];
 }
 
