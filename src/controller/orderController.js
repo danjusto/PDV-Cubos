357a3e0dc6fa44a652b2bd59e8a9e54f661020af
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-10-13 17:19:12 -0300

    refactor: excluse route detailOrder and implemented in listOrder with  query params

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -2,6 +2,9 @@ const { executeList, executeListById } = require('../services/orderService');
 const AppError = require('../errors/AppError');
 
 const listOrders = async (req, res) => {
+  const { id } = req.query;
+
+  if (!id) {
   try {
     const orders = await executeList();
     if (!orders) throw new AppError('orders not found', 404);
@@ -12,24 +15,20 @@ const listOrders = async (req, res) => {
     }
     return res.status(500).json({ message: 'Server error.' });
   }
-};
-
-const detailOrder = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const ordersClient = await executeListById(id);
-    if (!ordersClient) throw new AppError('orders not found', 404);
-    res.status(200).json(ordersClient);
-  } catch (error) {
-    if (error instanceof AppError) {
-      return res.status(error.statusCode).json({ message: error.message });
+  } else {
+    try {
+      const ordersClient = await executeListById(id);
+      if (!ordersClient) throw new AppError('orders not found', 404);
+      res.status(200).json(ordersClient);
+    } catch (error) {
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      return res.status(500).json({ message: 'Server error.' });
     }
-    return res.status(500).json({ message: 'Server error.' });
   }
 };
 
 module.exports = {
-  listOrders,
-  detailOrder,
+  listOrders
 };
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,12 +2,11 @@ const { Router } = require('express');
 const validateToken = require('../middlewares/validateToken');
 const validateIdParam = require('../middlewares/validateParam');
 const idParamSchema = require('../schemas/idParam');
-const { listOrders, detailOrder } = require('../controller/orderController');
+const { listOrders } = require('../controller/orderController');
 const routes = Router();
 
 routes.use(validateToken);
 
 routes.get('/', listOrders);
-routes.get('/:id', validateIdParam(idParamSchema), detailOrder);
 
 module.exports = routes;
