4db498d73ad85fb9835ce1ced003b0075b9e3b89
Daniel Justo <justodaniel29@gmail.com>
2023-10-14 10:55:53 -0300

    fix: query params validations

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,7 +5,7 @@ const validateBody = require('../middlewares/validateBody');
 const validateQueryParam = require('../middlewares/validateQueryParam');
 const productSchema = require('../schemas/product');
 const idParamSchema = require('../schemas/idParam');
-const idQueryParamSchema = require('../schemas/idQueryParam');
+const { categoriaIdQueryParam } = require('../schemas/idQueryParam');
 const { createProduct, listProducts, detailProduct, removeProduct, updateProduct } = require('../controller/productController');
 const multer = require('multer');
 
@@ -15,7 +15,7 @@ const upload = multer({});
 routes.use(validateToken);
 
 routes.post('/', upload.single('file'), validateBody(productSchema), createProduct);
-routes.get('/', validateQueryParam(idQueryParamSchema), listProducts);
+routes.get('/', validateQueryParam(categoriaIdQueryParam), listProducts);
 
 routes.use(validateParam(idParamSchema));
 routes.put('/:id', upload.single('file'), validateBody(productSchema), updateProduct);
--- src/schemas/idQueryParam.js
+++ src/schemas/idQueryParam.js
@@ -1,11 +1,19 @@
 const Joi = require('joi');
 
-const idQueryParam = Joi.object({
+const categoriaIdQueryParam = Joi.object({
   categoria_id: Joi.string()
     .pattern(/^[0-9]+$/)
     .messages({
-      'string.pattern.base': 'The param id must be a number',
+      'string.pattern.base': 'The query param categoria_id must be a positive number',
     }),
 });
 
-module.exports = idQueryParam;
+const clienteIdQueryParam = Joi.object({
+  cliente_id: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .messages({
+      'string.pattern.base': 'The query param cliente_id must be a positive number',
+    }),
+});
+
+module.exports = { categoriaIdQueryParam, clienteIdQueryParam };
