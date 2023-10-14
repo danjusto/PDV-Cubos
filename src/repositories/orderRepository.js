4d69c39618e04ea98b4de67b827f13e5d6a043c2
Daniel Justo <justodaniel29@gmail.com>
2023-10-14 10:44:41 -0300

    refactor: list orders

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -5,15 +5,11 @@ const insert = async (cliente_id, observacao, valor_total) => {
 };
 
 const findAllOrders = async () => {
-  return await knex('orders').select('id', 'valor_total', 'observacao', 'cliente_id');
+  return await knex('orders');
 };
 
 const findAllOrdersById = async (client_id) => {
-  return await knex('orders').select('id', 'valor_total', 'observacao', 'cliente_id').where('cliente_id', client_id);
+  return await knex('orders').where('cliente_id', client_id);
 };
 
-const findAllOrderProducts = async (idOrder) => {
-  return await knex('order_products').select('id', 'quantidade_produto', 'valor_produto', 'pedido_id', 'produto_id').where('pedido_id', idOrder);
-};
-
-module.exports = { insert, findAllOrders, findAllOrdersById, findAllOrderProducts };
+module.exports = { insert, findAllOrders, findAllOrdersById };
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -2,19 +2,16 @@ const AppError = require('../errors/AppError');
 const sendEmail = require('../utils/sendEmail.js');
 const { findById: findByIdClient } = require('../repositories/clientRepository.js');
 const { findById: findByIdProduct } = require('../repositories/productRepository.js');
-const { insert: insertOrder, findAllOrders, findAllOrdersById, findAllOrderProducts } = require('../repositories/orderRepository.js');
-const { insert: insertOrderProducts } = require('../repositories/orderProductsRepository');
+const { insert: insertOrder, findAllOrders, findAllOrdersById } = require('../repositories/orderRepository.js');
+const { insert: insertOrderProducts, findAllOrderProducts } = require('../repositories/orderProductsRepository');
 
 const executeCreate = async (cliente_id, observacao, produto_pedidos) => {
   const client = await findByIdClient(cliente_id);
   if (!client) {
     throw new AppError('Client not found.', 404);
   }
-
   let valor_total = 0;
-
   for (let produto of produto_pedidos) {
-    console.log(produto);
     const product = await findByIdProduct(produto.produto_id);
     if (!product) {
       throw new AppError('Product not found', 404);
@@ -26,9 +23,7 @@ const executeCreate = async (cliente_id, observacao, produto_pedidos) => {
 
     valor_total += product.valor * produto.quantidade_produto;
   }
-
   const newOrder = await insertOrder(cliente_id, observacao, valor_total);
-  console.log(newOrder);
   const listOfProducts = [];
 
   for (let produto of produto_pedidos) {
@@ -44,10 +39,11 @@ const executeCreate = async (cliente_id, observacao, produto_pedidos) => {
     orderProducts: listOfProducts,
   };
 };
-const executeList = async () => {
-  const listOrders = await findAllOrders();
-  const listOrdersProducts = [];
 
+const executeList = async (cliente_id) => {
+  let listOrders;
+  cliente_id ? (listOrders = await findAllOrdersById(cliente_id)) : (listOrders = await findAllOrders());
+  const listOrdersProducts = [];
   for (const order of listOrders) {
     const listProductsByOrder = await findAllOrderProducts(order.id);
     listOrdersProducts.push({
@@ -55,27 +51,7 @@ const executeList = async () => {
       pedido_produtos: listProductsByOrder,
     });
   }
-
   return listOrdersProducts;
 };
 
-const executeListById = async (id) => {
-  const listOrdersById = await findAllOrdersById(id);
-  const listOrdersProducts = [];
-
-  for (const order of listOrdersById) {
-    const listProductsByOrder = await findAllOrderProducts(order.id);
-    listOrdersProducts.push({
-      pedido: order,
-      pedido_produtos: listProductsByOrder,
-    });
-  }
-
-  return listOrdersProducts;
-};
-
-module.exports = {
-  executeCreate,
-  executeList,
-  executeListById,
-};
+module.exports = { executeCreate, executeList };
