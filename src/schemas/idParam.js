0754b1181bd1201748be49817f59f9cb7fb58166
Daniel Justo <justodaniel29@gmail.com>
2023-10-16 20:24:04 -0300

    finalizing project

--- src/schemas/idParam.js
+++ src/schemas/idParam.js
@@ -4,7 +4,7 @@ const idParam = Joi.object({
   id: Joi.string()
     .pattern(/^[0-9]+$/)
     .messages({
-      'string.pattern.base': 'The param id must be a number',
+      'string.pattern.base': 'The param id must be a number.',
     }),
 });
 
